d9bac14c86012d5d4bd961b1914b4ea1a6096d06
bellpornpawee <bellpornpawee40@gmail.com>
2025-10-17 22:03:45 +0700

    change port in cypress test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      config.baseUrl = 'http://localhost:80/'; // <--- แก้เป็น URL ของคุณ
+      config.baseUrl = 'http://localhost:5173/'; // <--- แก้เป็น URL ของคุณ
       codeCoverageTask(on, config);
       return config;
     },
